8a5c9feee00a027879c0f583ffb7de056ad0061e
Anup Bhowmik <1805082@ugrad.cse.buet.ac.bd>
2024-05-29 15:40:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  return {
+    skills: [
+      {
+        name: 'HTML',
+        level: '90%'
+      },
+      {
+        name: 'CSS',
+        level: '80%'
+      },
+      {
+        name: 'JavaScript',
+        level: '70%'
+      }
+    ]
+  };
+}
